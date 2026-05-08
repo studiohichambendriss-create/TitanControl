d7b65709876084a5e89ccf0ec858683df1045073
kner7 <hichaam_cool@hotmail.com>
2026-05-08 09:29:16 +0200

    Add download button and fast run script

--- run.bat
+++ run.bat
@@ -1,19 +1,11 @@
 @echo off
-setlocal
-set PY=python
-where python >nul 2>&1 || set PY=python3
-where python3 >nul 2>&1 || set PY=py
+echo STARTING...
 
-if "%PY%"=="" (
-    start chrome "file:///%CD%\index.html"
-    exit
-)
+:: Run server
+start "" /min python -m http.server 8001
 
-:: Clear port 8000
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :8000') do taskkill /f /pid %%a >nul 2>&1
+:: Open browser
+start chrome "http://localhost:8001"
 
-:: Start and Open
-start /b %PY% -m http.server 8000
-timeout /t 1 /nobreak >nul
-start chrome "http://localhost:8000"
+:: Gone
 exit
